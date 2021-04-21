17605b73c221134af2a535b2915b4ed6488d1b5d
sumanthpadakanti <sumanthpadakanti.psr@gmail.com>
2021-04-20 22:46:10 -0400

    added components, created http services with axios

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,39 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import eventList from "../views/eventList.vue";
+import eventShow from "../views/eventShow.vue";
+import eventCreate from "../views/eventCreate.vue";
+import user from "../views/user.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "event-list",
+    component: eventList,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/event/:id",
+    name: "event-show",
+    component: eventShow,
+    props: true
   },
+  {
+    path: "/event/create",
+    name: "event-create",
+    component: eventCreate,
+  },
+  {
+    path: "/user/:username",
+    name: "user-name",
+    component: user,
+    props: true
+  }
 ];
 
 const router = new VueRouter({
+  mode: "history",
   routes,
 });
 
---
+++ src/views/eventList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <EventCard v-for="event of events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventCard from '../components/EventCard';
+import EventService from '../services/EventService';
+export default {
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+    events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+    .then(response => {
+     this.events = response.data;
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+}
+</script>
